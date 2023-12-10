3fb57422b630337214f1d873b6b5880ea7bdc515
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2023-12-10 14:59:53 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-tabs>
+          <v-tab value="one">Item One</v-tab>
+          <v-tab value="two">Item Two</v-tab>
+          <v-tab value="three">Item Three</v-tab>
+        </v-tabs>
+      </v-col>
+      <v-col>
+        <v-btn>Button</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
